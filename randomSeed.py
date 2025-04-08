371f16e1b2a2ba15f5f7f1705a1a4ebc6e48e44e
Jutier <36769570+Jutier@users.noreply.github.com>
2025-04-08 19:44:42 -0300

    feat: feat: add function to create collages from hashes, kwargs

--- randomSeed.py
+++ randomSeed.py
@@ -1,10 +1,8 @@
 # Flake Maker by Jutier
 # Version: v1.2
 
-# This module generates snowflakes based on seeds,
-# can be used to replicated a snowflake or make a collage with multiple random snowflakes.
+# This module generates snowflakes based on seeds or hashes.
 
-import flake
 import hashlib
 from utils import interp
 import numpy as np
@@ -108,8 +106,44 @@ def img2Hash(image, pos):
 
 class PillowFlake(Snowflake):
 	"""
-	A subclass of Snowflake that adds functionality to draw lines on the canvas.
+	A subclass of Snowflake that implements drawLine method and adds some attributes.
+
+	Attributes:
+		image (PIL.Image): The image for the snowflake.
+		imgDraw (ImageDraw): The drawing context for the image.
 	"""
+	def __init__(self, **kwargs):
+		"""
+		Initializes a PillowFlake object with specified parameters.
+
+		Args:
+			size (int): The size of the resulting image. Defaults to 700.
+			bg_color (str): The background color of the image. Defaults to '#282923'.
+			**kwargs: Additional parameters passed to the parent class Snowflake.
+		"""
+
+		# Using pop is preferable over an optional parameter,
+		# it allows passing the value from distant functions.
+		size = kwargs.pop('size', 700)
+		bg_color = kwargs.pop('bg_color', '#282923')
+
+		default_params = {
+			'thickness': 1,
+			'growth': 20,
+			'max_cycles': 9,
+			'branch_crossing': True,
+			'color_young': '#2ab6e8',
+			'color_old': '#c0c4cf'
+		}
+
+		default_params.update(kwargs)
+		center = (size/2, size/2)
+
+		super().__init__(center, **default_params)
+		self.image = Image.new(mode='RGB', size=(size, size), color=bg_color)
+		self.imgDraw = ImageDraw.Draw(self.image)
+
+
 
 	def drawLine(self, line, points):
 		"""
@@ -118,78 +152,116 @@ class PillowFlake(Snowflake):
 		Args:
 			line (object): The line object containing color and thickness information.
 			points (tuple of tuple): The start and end points of the line to be drawn.
+		Returns:
+			None: This method modifies the image directly and does not return any value.
 		"""
-		self.canva.line(points, fill=line.color, width=int(line.thick)*2)
+		self.imgDraw.line(points, fill=line.color, width=int(line.thick)*2)
 
 
 
-def FlakeFromHash(sha, size):
+def flakeFromHash(sha, dt=1, **kwargs):
 	"""
 	Generates a flake image based on a SHA256 hash. 
 
 	Args:
 		sha (str): The SHA256 hash used to generate the flake.
-		size (int): The size of the resulting flake image.
+		dt (float): Delta used for the evolution. Values outside the range (0, growth) may result in unintended behavior. Defaults to 1.
+		**kwargs: Additional parameters passed to the PillowFlake class.
 
 	Returns:
 		PIL.Image: The generated flake image, with hash steganography.
 	"""
 	evoH = evoVal(sha[:32], 0, 100) # Extract the first 32 hex characters for humidity evolution
 	evoT = evoVal(sha[32:], -20, -5) # Extract the last 32 hex characters for temperature evolution
-	Flake = PillowFlake((size/2, size/2), 1, 20, 9, True, "#2ab6e8", "#c0c4cf")
 
-	image = Image.new(mode='RGB', size=(size, size), color='#282923')
-	canva = ImageDraw.Draw(image)
-	Flake.canva = canva # Assign the canvas to the PillowFlake object
+	Flake = PillowFlake(**kwargs)
 
 	for h, t in zip(evoH, evoT):
-		Flake.update(h, t, 1)
+		Flake.update(h, t, dt)
 
 	Flake.drawBranches()
 
-	steg_image = hash2Img(image, sha) # Embed the hash into the image
+	steg_image = hash2Img(Flake.image, sha) # Embed the hash into the image
 
 	return steg_image
 
-def FlakeCollage(seed, size, N):
+
+
+def flakeCollage(N, seed='random', **kwargs):
 	"""
-	Generates a collage of flake images based on a seed. Each flake is generated from a hash derived 
-	from the seed, and the resulting images are arranged in a grid.
+	Generates a collage of N flake.
+	Makes N hashes that are passed to collageFromHashList, those hashes are derived from the seed+salt.
 
 	Args:
-		seed (str): The input seed used to generate the hashes.
-		size (int): The size of each individual flake image.
 		N (int): The number of flakes along one dimension of the collage (grid size).
+		seed (str): The input seed used to generate the hashes, not really important.
+		**kwargs: Additional parameters passed to the collageFromHashList function.
 
 	Returns:
-		PIL.Image: The collage of flake images.
+		PIL.Image: The collage of flakes.
 	"""
-	collage = Image.new(mode='RGB', size=(size*N, size*N), color=None)
+	hash_list = [hashSeed(seed, True) for _ in range(N)]
 
-	for i in range(N):
-		for j in range(N):
-			flakeimg = FlakeFromHash(hashSeed(seed, False), size)
-			collage.paste(flakeimg, (size*i, size*j))
+	return collageFromHashList(hash_list, **kwargs)
+
+
+
+def collageFromHashList(hash_list, **kwargs):
+	"""
+	Creates a collage from a list of hashes, arranging the flakes in a grid.
+	If there aren't enough hashes to fill the grid, random flakes will be used.
+
+	Args:
+		hash_list (list): A list of hashes used to generate the flakes.
+		**kwargs: Additional parameters, such as rows, columns, size or others passed to the flakeFromHash function.
+
+	Returns:
+		PIL.Image: The collage of flakes.
+	"""
+	N = len(hash_list)
+	ceil = lambda n: -1 * int(-1 * n // 1) # Rounds up given number
+	rows = kwargs.pop('rows', ceil(N**(1/2))) # I believe this is a great default
+	columns = kwargs.pop('columns', rows)
+	size = kwargs.get('size', 700)
+	# Just for when I certainly forget: pop and get are being used for a reason
+
+	collage = Image.new(mode='RGB', size=(size*columns, size*rows), color=None)
+
+	h = 0
+	complete = True
+	for i in range(rows):
+		for j in range(columns):
+			if h < N:
+				flakeimg = flakeFromHash(hash_list[h], **kwargs)
+			else:
+				flakeimg = flakeFromHash(hashSeed('random', True), **kwargs)
+				complete = False
+			collage.paste(flakeimg, (size*j, size*i))
+			h += 1
+	if not complete:
+		print(f"The collage couldn't be filled with the provided hashes. {h - N} flakes were randomly generated.")
 
 	return collage
 
-def readCollage(image, N, flakes):
+
+
+def readCollage(image, rows, flakes):
 	"""
 	Extracts the hashes from a collage of flake images by reading the hash embedded in each flake image.
 
 	Args:
 		image (PIL.Image): The collage image from which to extract the hashes.
-		N (int): The number of flakes along one dimension of the collage (grid size).
-		flakes (list of int): A list of positions (1, N) representing the wanted flakes.
+		rows (int): The number of flake rows on the collage.
+		flakes (list): A list of The desired flakes  positions (row-wise).
 
 	Returns:
-		list of str: A list of extracted SHA256 hashes.
+		list: A list of extracted SHA256 hashes.
 	"""
 	hashList = []
-	size = int(image.size[0] / N) # Calculate the size of each flake image
+	size = int(image.size[1] / rows) # Calculate the size of each flake image
 
 	for p in flakes:
-		pos = (((p - 1) % N) * size, ((p - 1) // N) * size) # Calculate the position of the current flake in the collage
+		pos = (((p - 1) % rows) * size, ((p - 1) // rows) * size) # Calculate the position of the current flake in the collage
 		sha = img2Hash(image, pos)
 		hashList.append(sha)
 	return hashList
